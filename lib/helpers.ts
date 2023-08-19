a22ebcd06fbbbe5c86439e6f03f9d0e7729dbde3
MaarcusRenieroL <maarcusreniero@gmail.com>
2023-08-19 14:44:19 +0530

    API for users and categories added

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,4 +1,6 @@
 import prisma from "@/prisma";
+import { data } from "autoprefixer";
+import { NextResponse } from "next/server";
 
 export const connectToDb = async () => {
   try {
@@ -6,4 +8,30 @@ export const connectToDb = async () => {
   } catch (error: any) {
     throw new Error(error);
   }
+};
+
+export const generateSuccessMessage = (data: any, status: number) => {
+  return NextResponse.json(
+    {
+      message: "Success",
+      ...data,
+    },
+    {
+      status,
+      statusText: "Okay",
+    },
+  );
+};
+
+export const generateErrorMessage = (data: any, status: number) => {
+  return NextResponse.json(
+    {
+      message: "Error",
+      ...data,
+    },
+    {
+      status,
+      statusText: "Error",
+    },
+  );
 };
