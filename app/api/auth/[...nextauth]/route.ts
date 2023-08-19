6b3378dc1cfb05f60e9fc327ca90200041874fdc
MaarcusRenieroL <maarcusreniero@gmail.com>
2023-08-19 13:01:26 +0530

    Authentication added

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,68 @@
+import NextAuth, { AuthOptions } from "next-auth";
+
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDb } from "@/lib/helpers";
+import prisma from "@/prisma";
+import bcrypt from "bcrypt";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {
+          type: "text",
+        },
+        password: {
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials || !credentials.email || !credentials.password) {
+          return null;
+        }
+
+        try {
+          await connectToDb();
+
+          const user = await prisma.user.findFirst({
+            where: { email: credentials.email },
+          });
+
+          if (!user) {
+            return null;
+          }
+
+          if (!user.password) {
+            return null;
+          }
+
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password,
+          );
+
+          if (!isPasswordCorrect) {
+            return null;
+          }
+
+          return { ...user, id: user.id };
+        } catch (error) {
+          return null;
+        } finally {
+          await prisma.$disconnect();
+        }
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
 export default function Home() {
+  const { data, status } = useSession();
+
   return (
     <div>
       <h1>Hellow</h1>
+      <div>{JSON.stringify(data)}</div>
+      <div>{JSON.stringify(status)}</div>
     </div>
   );
 }
