6fa2e5e2eef7be2fa6263e89a6dde6715020a685
MaarcusRenieroL <maarcusreniero@gmail.com>
2023-08-19 18:49:40 +0530

    Screen sizes for responsiveness added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        xs: "300px",
+        sm: "600px",
+        md: "900px",
+        lg: "1200px",
+        "2xl": "1400px",
       },
     },
   },
