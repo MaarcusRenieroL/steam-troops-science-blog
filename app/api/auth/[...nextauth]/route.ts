ed362d6e4368acb1fd85938fdbce559b6ce45875
MaarcusRenieroL <maarcusreniero@gmail.com>
2023-08-21 16:00:29 +0530

    Google Authentication removed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
 import NextAuth, { AuthOptions } from "next-auth";
 
-import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { connectToDb } from "@/lib/helpers";
 import prisma from "@/prisma";
@@ -8,10 +7,6 @@ import bcrypt from "bcrypt";
 
 export const authOptions: AuthOptions = {
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    }),
     CredentialsProvider({
       name: "credentials",
       credentials: {
